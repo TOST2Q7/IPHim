a63696e20ced37adee685f86cd5db4c1faf0bf06
TOST2Q7 <tosteplay@gmail.com>
2024-02-11 23:04:09 +0700

    12398

--- index.js
+++ index.js
@@ -1,22 +1,83 @@
-var input_data = document.getElementById("input_data");
 
-function onChange() {
-  var value = e.value;
-  var text = e.options[e.selectedIndex].text;
-  console.log(value, text);
+
+
+function myFunction() {
+    alert("Функция вызвана!");
 }
 
+// Находим кнопку по ее id
+var button = document.getElementById("myButton");
+
+// Назначаем обработчик события "click" для кнопки
+button.addEventListener("click", myFunction);
 
 
 
 
-e.onchange = onChange;
 
 
 
-onChange();
 
 
+
+
+
+
+
+
+
+
+
+function checkSelected() {
+    const selectElement = document.getElementById("input_data").value;
+    const inputValue_Amper = document.getElementById("inputAmper").value;
+    const inputValue_Time = document.getElementById("inputTime").value;
+
+
+    
+
+
+    switch(selectElement) {
+        case "cu1":
+            handleOption1();    
+            break;
+        
+        case "cu2":
+            handleOption2();
+            break;
+        
+        case "al1":
+            handleOption3();
+            break;
+        
+        case "au1":
+            handleOption3();
+            break;
+
+        case "au2":
+            handleOption3();
+            break;
+        
+        default:
+            // Действие по умолчанию, если не выбрано ни одно из опций
+            break;
+    }
+}
+
+function handleOption1() {
+    console.log("Option 1 выбрана");
+    // Дополнительные действия для Option 1
+}
+
+function handleOption2() {
+    console.log("Option 1 выбрана");
+    // Дополнительные действия для Option 1
+}
+
+function handleOption3() {
+    console.log("Option 1 выбрана");
+    // Дополнительные действия для Option 1
+}
 // ---------------------------------
 
 
