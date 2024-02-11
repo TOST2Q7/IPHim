4f209d47774fedf323db20b1a44d2d55c304dec9
TOST2Q7 <tosteplay@gmail.com>
2024-02-11 19:45:47 +0700

    123

--- index.js
+++ index.js
@@ -1,26 +1,33 @@
-var elem = document.getElementById("anode-list-elements");
+var input_data = document.getElementById("input_data");
 
-
-
-elem.
-
-const myForm = document.myForm;
-
-const addButton = myForm.addButton, 
-    removeButton = myForm.removeButton, 
-    languagesSelect = myForm.language;
-
-// обработчик добавления элемента
-function addOption() {
-
-    // получаем текст для элемента
-    const text = myForm.textInput.value;
- 
-    // получаем значение для элемента
-    const value = myForm.valueInput.value;
- 
-    // создаем новый элемента
-    const newOption = new Option(text, value);
-    languagesSelect.options[languagesSelect.options.length]=newOption;
+function onChange() {
+  var value = e.value;
+  var text = e.options[e.selectedIndex].text;
+  console.log(value, text);
 }
 
+
+
+
+
+e.onchange = onChange;
+
+
+
+onChange();
+
+
+// ---------------------------------
+
+
+
+function getValue() {
+    // Получаем доступ к элементу input по его id
+    var inputElement = document.getElementById("myInput");
+    
+    // Получаем значение, введенное пользователем
+    var value = inputElement.value;
+    
+    // Выводим значение в консоль (или можете использовать его для других целей)
+    console.log("Значение введенное пользователем: " + value);
+}
